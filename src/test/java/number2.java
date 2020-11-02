aa5ddce7c86a255f7f10c0c7fd666d02c2a3dcb5
Karagoz <ayhanhulagu@gmail.com>
2020-11-01 22:33:04 -0500

    yeni bir cumle ekledim.

--- src/test/java/number2.java
+++ src/test/java/number2.java
@@ -3,7 +3,7 @@ public class number2 {
 
         System.out.println("Bye bye selenium");
         System.out.println("Happy cucumber!");
-        System.out.println(".........");
+        System.out.println(".........a");
 
 
     }
