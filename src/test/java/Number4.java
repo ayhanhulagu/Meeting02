1e6867152668e117fee0a25f7013842c4b79952f
Karagoz <ayhanhulagu@gmail.com>
2020-11-01 23:20:54 -0500

    add a new line : This is not line...

--- src/test/java/Number4.java
+++ src/test/java/Number4.java
@@ -3,5 +3,15 @@ public class Number4 {
         System.out.println("This is line");
 
         System.out.println("This is not line");
+
+
+
+
+
+        System.out.println("C");
+        System.out.println("O");
+        System.out.println("O");
+        System.out.println("L");
+
     }
 }
