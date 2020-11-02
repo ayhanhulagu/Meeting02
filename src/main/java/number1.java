a4a58281d842abc93760339c8986a7a2ff57f84c
Karagoz <ayhanhulagu@gmail.com>
2020-11-01 22:16:49 -0500

    yeni bir cumle ekledim.

--- src/main/java/number1.java
+++ src/main/java/number1.java
@@ -2,6 +2,8 @@ public class number1 {
 
     public static void main(String[] args) {
 
+        System.out.println("Hello earth!");
+
 
     }
 }
--- src/test/java/number2.java
+++ src/test/java/number2.java
@@ -1,5 +1,8 @@
 public class number2 {
     public static void main(String[] args) {
 
+        System.out.println("Bye bye selenium");
+        System.out.println("Happy cucumber!");
+
     }
 }
